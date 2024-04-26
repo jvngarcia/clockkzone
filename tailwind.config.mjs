86dc31a241f154af5e6c57b42a85154a33adf5f2
Angel Garcia <angelgarciaweb@gmail.com>
2024-04-26 15:50:23 -0400

    feat: add tailwind config

---
+++ tailwind.config.mjs
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		container: {
+			center: true,
+		},
+		extend: {
+			colors: {
+				'mine-shaft': {
+					'50': '#f6f6f6',
+					'100': '#e7e7e7',
+					'200': '#d1d1d1',
+					'300': '#b0b0b0',
+					'400': '#888888',
+					'500': '#6d6d6d',
+					'600': '#5d5d5d',
+					'700': '#4f4f4f',
+					'800': '#454545',
+					'900': '#3d3d3d',
+					'950': '#2e2e2e',
+				},
+			},
+		},
+	},
+	plugins: [],
+}
